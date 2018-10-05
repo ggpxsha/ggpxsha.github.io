75c45f2671b402edde66c07b761fbebaa2ae8b73
ggpxsha <43888213+ggpxsha@users.noreply.github.com>
2018-10-05 09:07:13 -0700

    Update and rename js.js to 23582.js

---
+++ 23582.js
@@ -0,0 +1,4 @@
+document.write('Let me tell you a secret! <button onclick="secret();">Secret</button>');
+function secret() {
+  document.write('You are a <span style="color:red";>GAYYYYYYYYYYY</span>!');
+}
