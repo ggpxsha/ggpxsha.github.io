7d39f0452a9fba4f617171991cd972bacf782239
ggpxsha <43888213+ggpxsha@users.noreply.github.com>
2019-04-06 23:18:53 -0700

    Delete 23582.js

--- 23582.js
+++
@@ -1,4 +0,0 @@
-document.write('Let me tell you a secret! <button onclick="secret();">Secret</button>');
-function secret() {
-  document.write('You are a <span style="color:red";>GAYYYYYYYYYYY</span>!');
-}
